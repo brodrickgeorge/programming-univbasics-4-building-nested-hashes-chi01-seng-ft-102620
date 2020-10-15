5a6ab24ba3e735d25596936b39a18707843b0719
Brodrick George <brodrick.george@yahoo.com>
2020-10-15 04:10:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,11 +11,11 @@ end
 
 def monopoly_with_second_tier
  new = {
-    :railroads => { 
-      :pieces => "4",
+    :railroads => [
+      :pieces => {"4"},
       :rent_in_dollars => {},
       :names => {}
-      }
+      ]
       
     }
 
